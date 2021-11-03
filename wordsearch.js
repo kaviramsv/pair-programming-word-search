e98cfd90d30d668c24f3db47e605f29b68117eaa
Kavita Ramesh <2706051+kaviramsv@users.noreply.github.com>
2021-11-03 17:16:14 -0400

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -14,30 +14,6 @@ const wordSearch = (letters, word) => {
     }
     return false;
 }
-    // for (l of vert) {
-    //     if (l.includes(word)){ 
-    //     return true
-    //     }
-    // }
-
-    // const horizontalJoin = letters.map(ls => ls.join(''))
-    // for (l of horizontalJoin) {
-    //     if (l.includes(word)){ 
-    //     return true
-    //     }
-    // }
-    // return false;
-
- wordSearch([
-    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-    ['S', 'E', 'E', 'R', 'F', 'E', 'L', 'D'],
-    ['Y', 'F', 'C', 'A', 'Q', 'U', 'A', 'L'],
-    ['H', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
-    ['W', 'H', 'C', 'K', 'Y', 'E', 'R', 'L'],
-    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-  ], 'FRANK')
+   
 
 module.exports = wordSearch
